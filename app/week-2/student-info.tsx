80933ce1199a659e9ba31fc159dad5ac1bf228f0
Xander Mulligan <xander.i.mulligan@gmail.com>
2026-01-20 14:45:13 -0700

    week 2

--- app/week-2/student-info.tsx
+++ app/week-2/student-info.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function StudentInfo() {
   return (
     <main>
       <h1>Xander Mulligan</h1>
+      <Link href={"https://github.com/Xander816M/cprg306-assignments"}>Link to my Github</Link>
     </main>
   );
 }
